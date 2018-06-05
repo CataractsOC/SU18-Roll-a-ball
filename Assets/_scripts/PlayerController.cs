9f3c9c1ea5dd8c5c41183a9f117cbc5efe7081ab
Josh Felipe Ursua <joshursua@airmail.cc>
2018-06-05 14:22:02 -0400

    Made Pickups Collectable
    Completed the "Collecting the Pick Up Objects" tutorial as detailed in SU18-Roll-a-ball/ProgressNotes/20180605_01-collectingPickupObjects.txt

--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -52,7 +52,7 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) 
 	{
 		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
-		if (other.gameObject.CompareTag ("Pick Up"))
+		if (other.gameObject.CompareTag ("pickup"))
 		{
 			// Make the other game object (the pick up) inactive, to make it disappear
 			other.gameObject.SetActive (false);
